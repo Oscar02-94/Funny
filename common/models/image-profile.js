bf52901f877fdb1ffa4a9b822c042e68a372f344
Benjamin De la Cruz MArtinez <chomi.numb.94@gmail.com>
2018-04-17 00:48:36 -0500

    Change for auth

--- common/models/image-profile.js
+++ common/models/image-profile.js
@@ -1,5 +1,13 @@
 'use strict';
 
 module.exports = function(Imageprofile) {
-
+  Imageprofile.updateImage = (imageId, url, next) => {
+    Imageprofile.updateAll({id: imageId}, {url: url})
+      .then(res => {
+        return next(null, res);
+      })
+      .catch(err => {
+        return next(err);
+      });
+  };
 };
